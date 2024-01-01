87dd897d9296f62187681b2a7de1612f81f5b274
Pavel <magma4810@gmail.com>
2024-01-01 10:32:38 +0300

    add tests

--- src/button.test.js
+++ src/button.test.js
@@ -1,12 +1,37 @@
 import { addButton } from "./button";
-
+import { showWeather } from "./weather";
+// import { cityCoordinates } from "./cityCoordinates";
 beforeEach(() => {
+  document.body.innerHTML = "";
   const list = document.createElement("div");
   list.className = "list";
-  document.append(list);
+  document.body.append(list);
+  const map = document.createElement("img");
+  map.className = "map";
+  document.body.append(map);
 });
-describe("test button", () => {
-  it("addButton", () => {
+jest.mock("./weather", () => {
+  return {
+    showWeather: jest.fn(),
+  };
+});
+jest.mock("./cityCoordinates", () => {
+  return {
+    cityCoordinates: jest.fn().mockResolvedValue([12.412, 41.412]),
+  };
+});
+describe("addButton", () => {
+  it("add button to list", () => {
+    addButton("London");
+    expect(
+      document.querySelector(".list").querySelector("button"),
+    ).toBeTruthy();
+  });
+  it("shows weather on click", () => {
     addButton("Rome");
+    const button = document.querySelector(".list button");
+    button.click();
+    expect(showWeather).toHaveBeenCalledWith("Rome");
+    console.log(document.body.innerHTML);
   });
 });
